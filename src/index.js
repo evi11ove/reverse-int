b87a2d66b196fca9b51f192e6dbba4efee1c0e93
Kirill <sahashik00@mail.ru>
2023-01-14 15:59:36 +0300

    feat: done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let result = '';
+    let nPos = Math.abs(n);
+    let str = String(nPos);
+    for(let i = str.length - 1; i >= 0 ; i--){
+        result = result + str[i];
+    }
+    return Number(result);
 }
